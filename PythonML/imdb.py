9676b8e0acfdf4b311a27b5f47658285263be799
tmendonca28 <tmendonca28@gmail.com>
2020-04-18 23:59:34 +0100

    Exploring imdb dataset
    Signed-off-by: tmendonca28 <tmendonca28@gmail.com>

---
+++ PythonML/imdb.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+imdb = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+# print(f"Training entries {len(train_data)}. Labels {len(train_labels)}")
+# print(len(train_data[0]), len(train_data[1]))
+
+word_index = imdb.get_word_index()
+
+word_index = {k: (v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2
+word_index["<UNUSED>"] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, "?") for i in text])
+
+print(decode_review(train_data[0]))
